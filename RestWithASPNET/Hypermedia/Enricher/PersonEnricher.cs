01e51db85038005ac7a5821a6e2eaeac433f8e70
Frwk Tiago Bezerra <frwk.tiago@unidas.com.br>
2023-05-16 17:52:33 -0300

    Add project files.

---
+++ RestWithASPNET/Hypermedia/Enricher/PersonEnricher.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestWithASPNETUdemy.Data.VO;
+using RestWithASPNETUdemy.Hypermedia.Constants;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Hypermedia.Enricher
+{
+    public class PersonEnricher : ContentResponseEnricher<PersonVO>
+    {
+        private readonly object _lock = new object();
+        protected override Task EnrichModel(PersonVO context, IUrlHelper urlHelper)
+        {
+            var path = "api/person/v1";
+            string link = GetLink(context.Id, urlHelper, path);
+
+            context.Links.Add(new HyperMediaLink()
+            {
+                Action = HttpActionVerb.GET,
+                Href = link,
+                Rel = ReletionType.self,
+                Type = ResponseTypeFormat.DefaultGet
+            });
+
+            context.Links.Add(new HyperMediaLink()
+            {
+                Action = HttpActionVerb.POST,
+                Href = link,
+                Rel = ReletionType.self,
+                Type = ResponseTypeFormat.DefaultPost
+            });
+
+            context.Links.Add(new HyperMediaLink()
+            {
+                Action = HttpActionVerb.PUT,
+                Href = link,
+                Rel = ReletionType.self,
+                Type = ResponseTypeFormat.DefaultPut
+            });
+
+            context.Links.Add(new HyperMediaLink()
+            {
+                Action = HttpActionVerb.PATCH,
+                Href = link,
+                Rel = ReletionType.self,
+                Type = ResponseTypeFormat.DefaultPatch
+            });
+
+            context.Links.Add(new HyperMediaLink()
+            {
+                Action = HttpActionVerb.DELETE,
+                Href = link,
+                Rel = ReletionType.self,
+                Type = "int"
+            });
+
+            return null;
+
+        }
+
+        private string GetLink(long id, IUrlHelper urlHelper, object path)
+        {
+            lock(_lock)
+            {
+                var url = new { controller = path, id = id };
+                return new StringBuilder(urlHelper.Link("DefaultApi", url)).Replace("%2F", "/").ToString();
+            }
+        }
+    }
+}
---
+++ RestWithASPNET/Model/Book.cs
@@ -0,0 +1,19 @@
+﻿using RestWithASPNETUdemy.Model.Base;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestWithASPNETUdemy.Model
+{
+    [Table("books")]
+    public class Book : BaseEntity
+    {
+
+        [Column("Author")]
+        public string Author { get; set; }
+
+        [Column("Price")]
+        public decimal Price { get; set; }
+
+        [Column("Title")]
+        public string Title { get; set; }
+    }
+}
